b5d9f4c52b91f41e2a2e0ecc35ca6593147f9eee
johnkasonda <john.kasonda@gmail.com>
2020-01-02 11:39:03 +0200

    Add project files.

---
+++ ClassLibrary2/Class1.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace ClassLibrary2
+{
+    public class Class1
+    {
+        public void Print(String input)
+        {
+            
+        }
+    }
+
+    public static class utility
+    {
+        public static void Print(string input)
+        {
+            Console.WriteLine(input);
+        }
+    }
+      
+
+    
+}
